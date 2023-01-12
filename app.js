768a7adbd13c39faeb35de1ac16f5fb321645a57
audu2110 <91955066+audu2110@users.noreply.github.com>
2023-01-12 12:21:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ const server=http.createServer((req,res)=>{
     console.log('OD');
 });
 server.listen(4000);
+
+
+
+const http= require('http');
+const server=http.createServer((req,res)=>{
+    console.log(req);
+});
+server.listen(4000);
+
+
