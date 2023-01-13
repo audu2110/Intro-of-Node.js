a4d8e5e15abb08bb8a8354d5c690e7470471b3a4
audu2110 <91955066+audu2110@users.noreply.github.com>
2023-01-13 14:53:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -70,3 +70,72 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const http = require('http');
+const fs = require('fs');
+let output_val=undefined;
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
+
+    fs.readFile('message.txt', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log(data);
+        output_val=data;
+    });
+    // console.log(output_val);
+
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title><head>');
+    res.write(`<body><p>${output_val}</p><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>`);
+    res.write('</html>');
+    
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+    });
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title><head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  res.write('</html>');
+  res.end();
+});
+
+server.listen(3000);
